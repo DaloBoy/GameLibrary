9b2fcef6745fb05694928f8d4c8492c4015e9029
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2016-11-24 12:55:00 +0100

    added Save and HentData Button and methods

--- GameLibrary/Model/obsGamesList.cs
+++ GameLibrary/Model/obsGamesList.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using Newtonsoft.Json;
 
 namespace GameLibrary.Model
 {
@@ -15,6 +16,26 @@ namespace GameLibrary.Model
             this.Add(new Game() { title = "The Witcher 3", pris = 280.0, dato = "19/5/2015", comment = "Single Player RPG. great story/slaying monster game 5/5 stars. "  });
             this.Add(new Game() { title = "Fallout 4", pris = 349.0, dato = "10/11/2015", comment = "Single player RPG sorymode game/Slaying Monster. 4/5 stars" });
 
+        }
+
+
+        /// <summary>
+        /// giver dig jsonformat Gameliste object
+        /// </summary>
+        /// <returns></returns>
+        public string GetJson()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+        public void indsætJson(String jsonText)
+        {
+            List<Game> nyListe = JsonConvert.DeserializeObject<List<Game>>(jsonText);
+
+            foreach ( var game in nyListe)
+            {
+                this.Add(game);
+            }
         }
     }
 }
--- GameLibrary/ViewModel/GameViewModel.cs
+++ GameLibrary/ViewModel/GameViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using Newtonsoft.Json;
+using Windows.Storage;
 
 namespace GameLibrary.ViewModel
 {
@@ -35,9 +37,21 @@ namespace GameLibrary.ViewModel
 
         public AddGameCommand AddGameCommand { get; set; }
         public DeleteGameCommand DeleteGameCommand { get; set; }
+        public SaveGameCommand SaveGameCommand { get; set; }
+        public HentDataCommand HentDataCommand { get; set; }
 
         public Model.Game NewGame { get; set; }
 
+        //public string GetGameListJson()
+        //{
+        //    string jsonText = JsonConvert.SerializeObject(Gameliste);
+        //    return jsonText;
+        //}
+
+
+        StorageFolder Localfolder = null;
+
+        private readonly string filnavn = "JsonText.Json";
 
         public GameViewModel()
         {
@@ -45,8 +59,33 @@ namespace GameLibrary.ViewModel
             AddGameCommand = new AddGameCommand(AddNewGame);
             NewGame = new Model.Game();
             DeleteGameCommand = new DeleteGameCommand(DeleteGame);
-            
+            SaveGameCommand = new SaveGameCommand(GemDataTilDiskAsync);
+
+            HentDataCommand = new HentDataCommand(HentdataFraDiskAsync);
+
+            Localfolder = ApplicationData.Current.LocalFolder;
+
             //AddGameCommand = new RelayCommand(AddNewGame,null);
+        }
+
+        public async void HentdataFraDiskAsync()
+        {
+            this.Gameliste.Clear();
+            StorageFile file = await Localfolder.GetFileAsync(filnavn);
+            string jsonText = await FileIO.ReadTextAsync(file);
+
+            Gameliste.indsætJson(jsonText);
+        }
+
+        /// <summary>
+        /// Gemmer HJson data fra liste i Localfolder
+        /// </summary>
+
+        public async void GemDataTilDiskAsync()
+        {
+            string jsonText = this.Gameliste.GetJson();
+            StorageFile file = await Localfolder.CreateFileAsync(filnavn, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(file, jsonText);
         }
 
         public void AddNewGame()
